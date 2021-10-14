64a6a87369f4f944279954961e05b450103deb22
batkomahno1 <45080834+batkomahno1@users.noreply.github.com>
2021-10-14 16:01:16 +0300

    Test WGAN small batches

--- config_test.py
+++ config_test.py
@@ -1,7 +1,7 @@
 DATASET=('mnist',)
 TGT_EPOCHS=(0,)
 PCT=(20,)
-EPS=(1.0,) # try zero here
+EPS=(0, 1.0,) # try zero here
 TARGETED=(True,)
 ATK = ('inf',)
 NOTE=('earlyStop',)
@@ -19,10 +19,10 @@ NOTE=('earlyStop',)
 # }
 
 # check if FIDs are same as during prototyping
-GAN_CHOICE = ['acgan']
+GAN_CHOICE = ['wgan']
 GAN_SETTINGS = {
 'acgan':(50,1000),
 'cgan':(100,500),
 'wgan_gp':(50,250),
-'wgan':(100,500//4)
+'wgan':(100,500//8)
 }
