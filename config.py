64617ac7830ce1ae04717cda0d3ce36ba0d27aae
batkomahno1 <45080834+batkomahno1@users.noreply.github.com>
2021-11-02 15:04:06 +0200

    config normal run

--- config.py
+++ config.py
@@ -1,23 +1,3 @@
-# # set experiment params
-# DATASET=('mnist','fmnist',)
-# TGT_EPOCHS=(0,)
-# PCT=(10, 20,)
-# EPS=(0.0, 1.0,)
-# TARGETED=(True,)
-# ATK = ('inf',) # TODO: add other options not implemented error
-# NOTE=('earlyStop','downgrade',)
-#
-# # select GAN archs to experiment
-# GAN_CHOICE = ['wgan', 'wgan_gp', 'cgan', 'acgan']
-#
-# # gan name : epochs, batch size
-# GAN_SETTINGS = {
-# 'acgan':(50,1000),
-# 'cgan':(100,500),
-# 'wgan_gp':(50,250),
-# 'wgan':(100,500)
-# }
-
 # set experiment params
 DATASET=('mnist','fmnist',)
 TGT_EPOCHS=(0,)
@@ -28,9 +8,29 @@ ATK = ('inf',) # TODO: add other options not implemented error
 NOTE=('earlyStop','downgrade',)
 
 # select GAN archs to experiment
-GAN_CHOICE = ['dpwgan',]
+GAN_CHOICE = ['wgan', 'wgan_gp', 'cgan', 'acgan']
 
 # gan name : epochs, batch size
 GAN_SETTINGS = {
-'dpwgan':(50,64//4),
+'acgan':(50,1000),
+'cgan':(100,500),
+'wgan_gp':(50,250),
+'wgan':(100,500)
 }
+
+# # set experiment params
+# DATASET=('mnist','fmnist',)
+# TGT_EPOCHS=(0,)
+# PCT=(10, 20,)
+# EPS=(0.0, 1.0,)
+# TARGETED=(True,)
+# ATK = ('inf',) # TODO: add other options not implemented error
+# NOTE=('earlyStop','downgrade',)
+#
+# # select GAN archs to experiment
+# GAN_CHOICE = ['dpwgan',]
+#
+# # gan name : epochs, batch size
+# GAN_SETTINGS = {
+# 'dpwgan':(50,64//4),
+# }
