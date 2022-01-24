a034e04edd780354626f12908aa76f2cd7280b06
batkomahno1 <45080834+batkomahno1@users.noreply.github.com>
2022-01-24 21:30:21 +0200

    calculated filtered fids

--- report.py
+++ report.py
@@ -94,3 +94,88 @@ for metric, metric_id in metric_map.items():
             # [tick.set_rotation(15) for tick in axs[i, idx].get_xticklabels()]
     plt.savefig('reports/'+source+'_'+metric+dp+'_report.png', bbox_inches="tight")
     plt.close()
+
+# GENERATE POST-FILTERING FIDS
+# quit if dp
+if opt.dp: sys.exit()
+
+# linearluy project tpr at 30% fpr
+tpr_list = {}
+for k in results.keys():
+    if not isinstance(k[1], str):# and k[1][3]>0:
+        fpr, tpr = [np.array(v) for v in [results[k][1][1], results[k][1][2]]]
+        if len(tpr)>0 and len(fpr)>0:
+            # print(tpr[np.where((fpr<=0.3) & (fpr>=0.2))[0]].max(), fpr[np.where((fpr<=0.3) & (fpr>=0.2))[0]].max())
+            y=tpr[np.where((fpr<=0.3) & (fpr>=0.2))[0]]
+            x=fpr[np.where((fpr<=0.3) & (fpr>=0.2))[0]]
+            z = np.polyfit(x, y, 1)
+            f = np.poly1d(z)
+            my_tpr = f(0.3).round(2).clip(0,1)
+            # print(k, )
+            p = k[1]
+            tpr_list[k[0],(p[2], p[3], p[5], 'inf', p[7])] = my_tpr
+    else:
+        tpr_list[k[0],k[1]] = 1.0
+tpr_list
+# project fid differences
+from statistics import median
+params_reduced=np.array(params_reduced)
+idxs_es = np.array([0,3,6])
+idxs_dg = np.array([0,2,5])
+idxs_ns = np.array([0,1,4])
+idxs_dict = {'downgrade':idxs_dg, 'earlyStop':idxs_es, 'noise':idxs_ns}
+decoys = 'downgrade', 'earlyStop', 'noise'
+calc_fid={}
+for g in gans:
+    for i, ds in enumerate(['fmnist','mnist']):
+        for dc in decoys:
+            medians = []
+            for p in params_reduced[idxs_dict[dc]+i*7]:
+                medians.append(median(accum[g,p][0]))
+            z = np.polyfit([0,10,20], np.array(medians), 1)
+            for p in params_reduced[idxs_dict[dc]+i*7]:
+                calc_fid[(g,p)] = np.poly1d(z)
+calc_fid['wgan_gp',(10, 1.0, 'mnist', 'inf', 'downgrade')](10)
+tpr_list['wgan_gp',(10, 1.0, 'mnist', 'inf', 'downgrade')]
+# median(accum['wgan_gp',(10, 1.0, 'mnist', 'inf', 'earlyStop')][0])-median(accum['wgan_gp','mnist'][0])
+calc_fid.keys()
+import matplotlib.pyplot as plt
+import numpy as np
+metric_map = {'fid':0, 'auc':1}
+metric, metric_id = 'fid', 0
+fig, axs = plt.subplots(len(gans), 2, figsize=(10,10), sharex=False)
+if len(axs.shape) < 2:
+    print('Reshaping axis...')
+    axs.shape = 1,2
+fig.autofmt_xdate(rotation=45)
+idx_d = lambda d: 0 if d=='mnist' else 1
+for i, g in enumerate(gans):
+    axs[i,0].set_ylabel(g.upper())
+    axs[0,0].set_title('mnist'.upper()+'\n n='+str(max(iterations)+1))
+    axs[0,1].set_title('fmnist'.upper()+'\n n='+str(max(iterations)+1))
+    x, y = ([],[]), ([],[])
+    for p in params_reduced:
+        # second condition filters out placeholders
+        # if not isinstance(p,str) and max(accum[g,p][metric_id]) > 0:
+        dp_skip = False#opt.dp and metric=='fid' and isinstance(p,str)
+        if max(accum[g,p][metric_id]) > 0 and not dp_skip:
+            d = p if isinstance(p,str) else p[2]
+            x_label = str(p[:2] + p[-2:]) if not isinstance(p,str) else 'clean'
+            x[idx_d(d)].append(x_label)
+            if isinstance(p, str):
+                y[idx_d(d)].append(accum[g,p][metric_id])
+            else:
+                new_pct = (1-tpr_list[g,p])*p[0]
+                temp = np.array(accum[g,p][metric_id])*calc_fid[g,p](new_pct)/median(accum[g,p][metric_id])
+                y[idx_d(d)].append(temp)
+            # fid_cln = median(accum[g,d][metric_id])
+            # diff = (np.array(accum[g,p][metric_id])-fid_cln)*(1-tpr_list[g,p])
+            # y[idx_d(d)].append(fid_cln+diff)
+    for idx in [0,1]:
+        # print(x,y)
+        axs[i, idx].boxplot(y[idx], labels=x[idx], notch=True)
+        # [tick.set_rotation(15) for tick in axs[i, idx].get_xticklabels()]
+plt.savefig('reports/'+source+'_'+metric+dp+'_report_filtered.png', bbox_inches="tight")
+plt.close()
+
+params_reduced
